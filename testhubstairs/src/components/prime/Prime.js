8a1ab6bc232eb19d567e306d878bc4f1a20659fa
Cyril Napolitano <cyril.napolitano42@gmail.com>
2018-06-26 20:54:47 +0200

    v0.1 page prime

--- testhubstairs/src/components/prime/Prime.js
+++ testhubstairs/src/components/prime/Prime.js
@@ -6,10 +6,18 @@ class Prime extends Component {
         super();
         this.state = {
             userInput: '',
+            primaryNumbers: [],
+            inputNumber: 'x',
         }
     };
 
+    //récupéré la valeur des nombres premiers
+    //une fois l'application monté
+    componentWillMount() {
+        this.getPrimaryNumbers()
+    }
 
+    //récupéré la valeur des nombres premiers
     getPrimaryNumbers(){
         let max = 10000;
         let primaryNumber = []
@@ -23,40 +31,70 @@ class Prime extends Component {
           } while(j <= racine && i%j !== 0)
           if(j > racine) {
            primaryNumber.push(i)
-           console.log(primaryNumber)
+           this.state.primaryNumbers.push(i)
           }
         }
+        //  this.setState(previousState => ({
+        //   primaryNumbers: [...previousState.primaryNumbers, primaryNumber]
+        // }))
+    }
+
+
+    //supprimer la valeur de l'input 
+    //setter-stocker le la valeur de l'input valider
+    onSubmit(event) {
+        event.preventDefault();
+        this.setState({
+            userInput: '',
+            inputNumber: this.state.userInput,
+        });
+    }
+
+    //Recupéré la valeur de l'input
+    onChange(event) {
+        this.setState({
+            userInput: event.target.value
+        });
+    }
+
+    //Affiché le résultat
+    renderPrimaryNumber() {
+        console.log(this.state)
+        return (
+        <div className="p-2">
+            { "Le " + this.state.inputNumber + "ième nombre premier est : " + this.state.primaryNumbers[this.state.inputNumber]}
+        </div> )
     }
     
 
-  render() {
-    return (
-    <div>
-      <div className="prime container">
-        Prime
-      </div>
-
-     <h3>Cette calculatrice permet de trouver n-ième nombre premier</h3>
-     <input 
-        value={this.state.userInput} 
-        type="text" 
-        placeholder="Renseigner un nombre inférieur à 1000"
-        // onChange={/* Recupéré la valeur de l'input */}
-        className="form-control mb-2"
-      />
-      <button 
-        // onClick={/* Renvoyer le résultat sur la page */} 
-        className="btn btn-primary"
-      >
-        Valider
-      </button>
-
-       <div className="lead">
-         {/* Renvoyer nombre premier */}
+    render() {
+        return (
+        <div>
+        <div className="prime container">
+            Prime
         </div>
-    </div>
-    );
-  }
+
+        <h3>Cette calculatrice permet de trouver n-ième nombre premier</h3>
+        <input 
+            value={this.state.userInput} 
+            type="text" 
+            placeholder="Renseigner un nombre inférieur à 1000"
+            onChange={this.onChange.bind(this)}
+            className="form-control mb-2"
+        />
+        <button 
+            onClick={this.onSubmit.bind(this)} 
+            className="btn btn-primary"
+        >
+            Valider
+        </button>
+
+        <div className="lead">
+            {this.renderPrimaryNumber()}
+        </div>
+        </div>
+        );
+    }
 }
 
 export default Prime;
