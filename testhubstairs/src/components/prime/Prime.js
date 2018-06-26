51adec1bb0477e1659819771f2df64585ef775ff
Cyril Napolitano <cyril.napolitano42@gmail.com>
2018-06-26 20:32:38 +0200

    ajout de l'algo nombre premier

--- testhubstairs/src/components/prime/Prime.js
+++ testhubstairs/src/components/prime/Prime.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import '../../App.css';
 
 class Prime extends Component {
+    constructor() {
+        super();
+    };
+
+
+    getPrimaryNumbers(){
+        let max = 10000;
+        let primaryNumber = []
+        let i = 2;
+      
+        for(i; i <= max; i++) {
+          let j = 1;
+          let racine = Math.floor(Math.sqrt(i))
+          do {
+            j++;
+          } while(j <= racine && i%j !== 0)
+          if(j > racine) {
+           primaryNumber.push(i)
+           console.log(primaryNumber)
+          }
+        }
+    }
+    
+
   render() {
     return (
       <div className="prime">
